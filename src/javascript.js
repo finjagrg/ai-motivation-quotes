271d98e2b5a6ae6214143a58b115e01a312103e2
finjagrg <finjagoergg@gmail.com>
2024-03-03 17:34:33 +0100

    added button functionality with javascript

--- src/javascript.js
+++ src/javascript.js
@@ -1,1 +1,13 @@
+function displayQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem-container", {
+    strings:
+      "This is a wonderful motivation quoteGenerator, and thsi is a test sentence",
+    autoStart: true,
+    deplay: 60,
+  });
+}
+
+let quoteGenerator = document.querySelector("#enter-topic");
+quoteGenerator.addEventListener("submit", displayQuote);
