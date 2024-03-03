0ab1c13cdc611a7f74aebfae8eda2f3d9c4d5f98
finjagrg <finjagoergg@gmail.com>
2024-03-03 19:06:37 +0100

    finalized the AI prompt

--- src/javascript.js
+++ src/javascript.js
@@ -4,7 +4,7 @@ function writeQuote(response) {
   new Typewriter("#poem-container", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 1,
+    delay: 10,
     cursor: null,
   });
 }
@@ -14,7 +14,7 @@ function makeApiCall(event) {
   let topic = document.querySelector("#topic");
 
   let apiKey = "0to1c5e82ab3ce410bae0704083170ff";
-  let prompt = `Write a quote about ${topic.value}. It should not be longer than four lines`;
+  let prompt = `Write a quote about ${topic.value}. It should not be longer than four lines. Write the german translation after a big paragraph below the english one`;
   let context =
     "You are a mixture of Tony Robbins, Michelle Obama, Steven R. End every answer with '<br /><em>Quote by <strong>Finja G.</strong></em>'. Behave to this rules";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
