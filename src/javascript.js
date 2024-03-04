e902157cdf568aa27a84ac8a9d757748e0f5b3c5
finjagrg <finjagoergg@gmail.com>
2024-03-04 20:14:21 +0100

    sharped the AI context and hide quoteBox until something is typed

--- src/javascript.js
+++ src/javascript.js
@@ -14,13 +14,17 @@ function makeApiCall(event) {
   let topic = document.querySelector("#topic");
 
   let apiKey = "0to1c5e82ab3ce410bae0704083170ff";
-  let prompt = `Write a quote about ${topic.value}. It should not be longer than four lines. Write the german translation after a big paragraph below the english one`;
+  let prompt = `Please write a quote about ${topic.value}.`;
   let context =
-    "You are a mixture of Tony Robbins, Michelle Obama, Steven R. End every answer with '<br /><em>Quote by <strong>Finja G.</strong></em>'. Behave to this rules";
+    "You are a mixture of Tony Robbins, Michelle Obama, Steven R. Your answer should not be longer than four lines. Write the english answer, and right below the german translation of it. End your answer with '<br /><em>Quote by <strong>Finja G.</strong></em>' at the very END of your answer. Exampe: 'Flower <br />Blumen <br /> Quote by <strong>Finja G.</strong>";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiURL).then(writeQuote);
   console.log(prompt);
+
+  let quoteBox = document.querySelector("#poem-container");
+  quoteBox.classList.remove("hidden");
+  quoteBox.innerHTML = `<div class="blink">⌛️ Generating a Poem about ${topic.value} for you</div>`;
 }
 
 let quoteGenerator = document.querySelector("#enter-topic");
